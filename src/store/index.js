06461aebf104d2b1253e1ffc8e9e84f86d00115d
MingzhePei <540191212@qq.com>
2020-11-18 21:06:18 +0800

    引入bootstrapvue 新增导航栏

--- src/store/index.js
+++ src/store/index.js
@@ -5,8 +5,21 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    status: "",
+    token: "",
+    username: ""
   },
   mutations: {
+    login(state) {
+      state.status = "success"
+      state.token = "123456"
+      state.username = "testUser"
+    },
+    logout(state) {
+      state.status = ""
+      state.token = ""
+      state.username = ""
+    }
   },
   actions: {
   },
---
+++ src/views/Navbar.vue
@@ -0,0 +1,79 @@
+<template>
+  <div>
+    <b-navbar toggleable="lg" type="dark" variant="dark" class="shadow">
+      <b-navbar-brand to="/">
+        <img src="../../public/logoNav.png">
+      </b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav v-for="item in navItems" :key="item.id">
+          <b-nav-item :to="item.route" class="navItemCustom">{{ item.title }}</b-nav-item>
+        </b-navbar-nav>
+
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto" v-if="!hasLoginIn">
+          <b-nav-item class="navItemCustom" @click="fakeLogin">登录</b-nav-item>
+          <b-button size="lg" class="navButtonCustom" to="">注册</b-button>
+        </b-navbar-nav>
+
+        <b-navbar-nav class="ml-auto" v-else>
+          <b-nav-item class="navItemCustom">{{ userName }}</b-nav-item>
+          <b-nav-item class="navItemCustom" @click="fakeLogout">
+            <b-icon-power></b-icon-power>
+          </b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+  data() {
+    return {
+      hasLoginIn: false,
+      userName: "",
+      navItems:[{
+        id: 0,
+        title: "首页",
+        route: "/"
+      },{
+        id: 1,
+        title: "歌曲收录",
+        route: "Collection"
+      },{
+        id: 2,
+        title: "待定",
+        route: "/"
+      }]
+    }
+  },
+  methods: {
+    fakeLogin() {
+      this.$store.commit('login')
+      this.hasLoginIn = !this.hasLoginIn
+      this.userName = this.$store.state.username
+    },
+    fakeLogout() {
+      this.$store.commit('logout')
+      this.hasLoginIn = !this.hasLoginIn
+      this.userName = ""
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .navItemCustom {
+    font-size: 20px;
+    margin: 0 15px 0 15px;
+  }
+  .navButtonCustom {
+    background-color: #00A8E9;
+    margin-left: 20px;
+    width: 100px;
+  }
+</style>
